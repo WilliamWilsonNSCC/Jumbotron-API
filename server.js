47e71ff023c59658aa710c390120e14623cd7fdb
WilliamWilsonNSCC <w0457754@campus.nscc.ca>
2025-11-04 12:22:32 -0400

    Added comments

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+
 const port = process.env.PORT || 3000;
 const app = express();
+
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//Routes (API endpoints)
 app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
+
+//Server starting
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
